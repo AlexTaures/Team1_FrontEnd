7e9f0a9ee1df6221973bca7fbb2adc95d2534171
Alex Diaz <alextauresdiaz@gmail.com>
2023-03-02 19:24:25 -0600

    Close to end

--- src/components/Shopping-cart/ShoppingPay.jsx
+++ src/components/Shopping-cart/ShoppingPay.jsx
@@ -1,18 +1,54 @@
-import React, { useContext } from 'react'
-import { DataContext } from '../../context/Datacontext'
+import axios from 'axios';
+import React, { useContext } from 'react';
+import { DataContext } from '../../context/Datacontext';
+import routes from "../../connection/BackendRoutes.json";
+import { useNavigate } from 'react-router-dom'
 
 export default function ShoppingPay() {
+  const navigate = useNavigate();
+  const { cart, setCart, userInfo } = useContext(DataContext);
+  let productList = [];
   let total = 0;
-  const { cart } = useContext(DataContext)
 
   if(cart.items.length>0){
     cart.items.forEach(element => {
       total += element.amount*element.price;
     });
   }
+
+  const payCart = async () => {
+    cart.items.forEach((e, key)=> {
+      productList.push(`'key': ${key+1},'name': ${e.name},'price': ${Math.floor(e.price).toFixed(2)},'amount': ${e.amount},'total': ${Math.floor(e.amount*e.price).toFixed(2)}`)
+    });
+
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let currentDate = `${year}-${month}-${day}`;
+
+   try {
+          
+      const url = routes['sales'];
+      const body = {
+        "products": productList.toString(),
+        "sale_date": currentDate,
+        "total_sale": total,
+        "customer_id": userInfo["id"]
+      }
+      
+      await axios.post(url, body);
+      setCart({items: [],total: 0});
+      return (navigate("/shopping"));
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="generalContainer">
-      <div className="subContainer"></div>
+      <div className="subContainer">
       <table>
         <thead>
           <tr>
@@ -29,15 +65,17 @@ export default function ShoppingPay() {
               <tr key={key}>
                 <td>{key+1}</td>
                 <td>{item.name}</td>
-                <td>{item.price}</td>
+                <td>${Math.floor(item.price).toFixed(2)}</td>
                 <td>{item.amount}</td>
-                <td>${item.amount*item.price}</td>
+                <td>${Math.floor(item.amount*item.price).toFixed(2)}</td>
               </tr>
             ))
           }
-          <td>Total</td> <td></td> <td></td> <td></td> <td>${total}</td>
+          <td>Total</td> <td></td> <td></td> <td></td> <td>${total.toFixed(2)}</td>
         </tbody>
       </table>
+      <button className='payButton px-3' onClick={payCart}>Pay Cart</button>
+      </div>      
     </div>
   )
 }
